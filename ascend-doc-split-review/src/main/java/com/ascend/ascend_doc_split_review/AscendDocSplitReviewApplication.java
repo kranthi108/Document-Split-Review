fdfbd5477456781d6025d9304871f12aa9e713d4
Kranthi Ramireddy <kranthiramireddy@gmail.com>
2025-11-11 17:39:06 +0530

    initial commit for spring initializer

---
+++ ascend-doc-split-review/src/main/java/com/ascend/ascend_doc_split_review/AscendDocSplitReviewApplication.java
@@ -0,0 +1,13 @@
+package com.ascend.ascend_doc_split_review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AscendDocSplitReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AscendDocSplitReviewApplication.class, args);
+	}
+
+}
---
+++ ascend-doc-split-review/src/test/java/com/ascend/ascend_doc_split_review/AscendDocSplitReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ascend.ascend_doc_split_review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AscendDocSplitReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
